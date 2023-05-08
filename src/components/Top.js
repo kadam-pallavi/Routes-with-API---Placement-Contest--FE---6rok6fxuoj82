1c8e8fbb4848f7759df32d9aecd2f9087f6b8f95
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-05-08 22:27:00 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,39 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-
 function Top() {
     const [cryptoArr, setCryptoArr] = useState([]);
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('https://api.coinlore.net/api/tickers/')
-            .then((res)=>{
-                return res.json();
+            .then(response => response.json())
+            .then(data => {
+                setCryptoArr(data.data.slice(0, 10));
             })
-            .then((res)=>{
-                // console.log(res.data);
-                setCryptoArr(res.data);
-            })
-    },[])
+            .catch(error => console.log(error));
+    }, []);
 
     return (
         <div className='top-container'>
             <h1>Top 10 Global Crypto Coins</h1>
             <div className='display-container'>
-                {   
-                    cryptoArr.slice(0,10).map((data)=>{
-                        
-                        return (
-                            <div className='coin-container'>
-                                <p className='rank-para'>Rank #{data.rank}</p>
-                                <h3 className='coin-symbol'>{data.name}({data.symbol})</h3>
-                                <p className='price-para'>Price: {data.price_usd}</p>
-                            </div>
-                        )
-                    })
-                }
-                
+                {cryptoArr.map(crypto => (
+                    <div key={crypto.id} className='coin-container'>
+                        <p className='rank-para'>Rank #{crypto.rank}</p>
+                        <h3 className='coin-symbol'>{crypto.name} ({crypto.symbol})</h3>
+                        <p className='price-para'>Price: {crypto.price_usd}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
 }
 
-export default Top
+export default Top;
